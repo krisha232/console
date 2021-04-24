0119e2760cbe3b93b311c7ccea37564a2d85b859
krisha232 <78255450+krisha232@users.noreply.github.com>
2021-04-24 11:38:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var f=54
+var d=677
+var n="krisha"
+var s="sureka"
+var ns=n.concat(s);
+console.log(ns);
+console.log(n);
+console.log(s);
+console.log(f);
+console.log(d);
+console.log(f+d);
+console.log(f-d);
+console.log(f*d)
+console.log(f/d);;
